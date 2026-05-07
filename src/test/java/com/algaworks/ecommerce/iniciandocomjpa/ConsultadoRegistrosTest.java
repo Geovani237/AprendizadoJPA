94b8e6afd975fa2c47889db38b6814f68366b6e7
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-07 16:09:30 -0300

    Buscando objetos por identificador

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultadoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultadoRegistrosTest.java
@@ -1,12 +1,10 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import com.algaworks.ecommerce.model.Produto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 public class ConsultadoRegistrosTest {
 
@@ -33,5 +31,28 @@ public class ConsultadoRegistrosTest {
     @AfterEach
     public void tearDown() {
         entityManager.close();
+    }
+
+    @Test
+    public void buscarPorIdentificador() {
+        Produto produto = entityManager.find(Produto.class, 1);
+//        Produto produto = entityManager.getReference(Produto.class, 1);
+
+        System.out.println(">>> " + produto.getNome() +  " <<<");
+//        System.out.println("Ainda não buscou!! ");
+
+        Assertions.assertNotNull(produto);
+        Assertions.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void atualizarAReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("PS5 Pro");
+
+        entityManager.refresh(produto);
+
+
+        Assertions.assertEquals("Kindle", produto.getNome());
     }
 }
