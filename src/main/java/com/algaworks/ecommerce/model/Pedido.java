078bac5896b1ba23ef972e89901268904f128cec
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-12 11:09:15 -0300

    Mapeando as entidades e customizando os nomes das tabelas e colunas

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -1,7 +1,9 @@
 package com.algaworks.ecommerce.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,21 +16,26 @@ import java.time.LocalDateTime;
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "pedido")
 public class Pedido {
 
     @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
+    @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
 
+    @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
+    @Column(name = "nota_fiscal_id")
     private Integer notaFiscalId;
 
     private BigDecimal total;
 
     private StatusPedido status;
 
+    @Column(name = "categoria_pai_id")
     private Integer categoraiPaiId;
 }
