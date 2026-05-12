46e46841a329e8e951f41d72082ee7f9ce650e35
Geovani237 <geovanicsouza7@gmail.com>
2026-05-11 21:42:46 -0300

    exercício de CRUD

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/PrimeiroCrudTest.java
@@ -0,0 +1,12 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+
+public class PrimeiroCrudTest extends EntityManagerTest {
+
+    //TODO inserirRegistro
+    //TODO buscarPorIdentificado
+    //TODO atualizarRegistro
+    //TODO removerRegistro
+
+}
