fdb5dc62eaeb9135fb1294535b282ff280d0b381
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-12 16:16:46 -0300

    Mapeando relacionamentos um-para-muitos com @OneToMany

--- src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -19,15 +19,9 @@ public class ItemPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-//    @Column(name = "pedido_id")
-//    private Integer pedidoId;
-
     @ManyToOne
     @JoinColumn(name = "pedido_id")
     private Pedido pedido;
-
-//    @Column(name = "produto_id")
-//    private Integer produtoId;
 
     @ManyToOne
     @JoinColumn(name = "produto_id")
