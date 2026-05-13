ca6b3c441d47c883e0c7edde005eced51a158257
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-13 15:41:40 -0300

    Gerenciamento de transações

---
+++ src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/GerenciamentoTransacaosTest.java
@@ -0,0 +1,36 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPedido;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GerenciamentoTransacaosTest extends EntityManagerTest {
+
+    @Test
+    public void abrirFecharCancelarTransacao() {
+        Assertions.assertThrows(Exception.class, this::erroEsperadoMetotoDeNegocio);
+    }
+
+    private void erroEsperadoMetotoDeNegocio() {
+        try {
+            entityManager.getTransaction().begin();
+            metodoDeNegocio();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+
+    private void metodoDeNegocio() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+        pedido.setStatus(StatusPedido.PAGO);
+
+        if (pedido.getPagamento() == null) {
+            throw new RuntimeException("Pedido ainda não foi pago.");
+        }
+    }
+}
