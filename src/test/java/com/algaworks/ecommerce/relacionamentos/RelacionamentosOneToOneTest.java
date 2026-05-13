01493687a827d4d88f89723f3b0d185fe6fb14fc
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-13 10:27:25 -0300

    Mapeamento relacionamentos um-para-um com @OneToOne

---
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
@@ -0,0 +1,31 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class RelacionamentosOneToOneTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setNumero("1234");
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pagamentoCartao);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificado = entityManager.find(Pedido.class, pedido.getId());
+        Assertions.assertNotNull(pedidoVerificado.getPagamento());
+
+    }
+}
