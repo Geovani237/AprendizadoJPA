da619ad8f634b6ac509856c870028c894eacf6bb
Geovani237 <geovanicsouza7@gmail.com>
2026-05-18 16:31:01 -0300

    Mapeando coleções de objetos embutidos com @ElementCollection

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.mapeamentoavancado;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Atributo;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,20 @@ public class ElementCollectionTest extends EntityManagerTest {
         Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
 
         Assertions.assertFalse(produtoVerificado.getTags().isEmpty());
+    }
+
+    @Test
+    public void aplicarAtributos() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertFalse(produtoVerificado.getAtributos().isEmpty());
     }
 }
