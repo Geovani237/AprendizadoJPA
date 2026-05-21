cdd5006984f7d8d68997e6af61269b215ec639c9
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-21 10:57:52 -0300

    Exercícios sobre merge

--- src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
+++ src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
@@ -5,10 +5,37 @@ import com.algaworks.ecommerce.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 public class CascadeTypeMergeTest extends EntityManagerTest {
+
+//    @Test
+    public void atualizarProdutoComCategoria() {
+        Produto produto = new Produto();
+        produto.setId(1);
+        produto.setDataUltimaAtualizacao(LocalDateTime.now());
+        produto.setPreco(new BigDecimal("500"));
+        produto.setNome("Kindle");
+        produto.setDescricao("Agora com iluminação embutida ajustável.");
+
+        Categoria categoria = new Categoria();
+        categoria.setId(2);
+        categoria.setNome("Tablets");
+
+        produto.setCategorias(List.of(categoria)); // CascadeType.MERGE
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assertions.assertEquals(categoria.getNome(), categoriaVerificacao.getNome());
+    }
 
     //@Test
     public void atualizarPedidoComItens() {
