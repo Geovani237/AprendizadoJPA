381a3fec6c21f903d5d87d705d22a0812edb6959
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-05-26 13:18:48 -0300

    Passando parâmetros para as consultas

--- src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,17 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class PathExpressionTest extends EntityManagerTest {
-    
+
+    @Test
+    public void buscarPedidoComProdutoEspecifico() {
+        String jpql = "select i.pedido from ItemPedido i where i.id.produtoId = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> pedido = typedQuery.getResultList();
+        Assertions.assertEquals(2, pedido.size());
+
+    }
+
     @Test
     public void usarPathExpressions() {
         String jpql = "select p.cliente.nome from Pedido p";
