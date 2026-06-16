9b0ac609d9fbed550f5ab7b0555ed51af42b6467
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-06-16 09:27:34 -0300

    Entendendo o cache de segundo nível

---
+++ src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
@@ -0,0 +1,40 @@
+package com.algaworks.ecommerce.cache;
+
+import com.algaworks.ecommerce.model.Pedido;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CacheTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void buscarDoCache() {
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Buscando a partir da instância 1: ");
+        entityManager1.find(Pedido.class, 1);
+
+        System.out.println("Buscando a partir da instância 2: ");
+        entityManager2.find(Pedido.class, 1);
+    }
+
+
+}
