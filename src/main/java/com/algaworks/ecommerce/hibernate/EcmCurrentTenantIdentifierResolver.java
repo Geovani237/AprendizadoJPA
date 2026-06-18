f19e7f8719e06f6579fb64af58377637cc437a4e
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-06-18 09:59:45 -0300

    Implementando multitenancy com abordagem por schema

---
+++ src/main/java/com/algaworks/ecommerce/hibernate/EcmCurrentTenantIdentifierResolver.java
@@ -0,0 +1,30 @@
+package com.algaworks.ecommerce.hibernate;
+
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+
+public class EcmCurrentTenantIdentifierResolver implements CurrentTenantIdentifierResolver {
+
+    private static ThreadLocal<String> tl = new ThreadLocal<>();
+
+    /*
+    O ThreadLocal em Java serve para criar variáveis com escopo restrito
+    a uma única thread. Ele permite que diferentes threads acessem a mesma
+    variável, mas cada uma enxerga e manipula um valor isolado e
+    independente, sem interferir nos dados das outras e sem precisar de
+    sincronização manual (locks).
+     */
+
+    public static void setTenantIdentifier(String tenantId) {
+        tl.set(tenantId);
+    }
+
+    @Override
+    public String resolveCurrentTenantIdentifier() {
+        return tl.get();
+    }
+
+    @Override
+    public boolean validateExistingCurrentSessions() {
+        return false;
+    }
+}
